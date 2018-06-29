91d24c6f43f8d53f422fe2e39a20f7833e93c47b
Lucas Tiedemann <lucas95t@gmail.com>
2018-06-29 09:47:52 +0200

    Datenbank Verbindung (env)

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'projektserver5.as.wiwi.uni-goettingen.de:22'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'mob182db'),
+            'username' => env('DB_USERNAME', 'mob182'),
+            'password' => env('DB_PASSWORD', 'royoludecoxo'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
